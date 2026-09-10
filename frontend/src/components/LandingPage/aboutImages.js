350a72af54fbb77bae5aa098282682b5c8862025
CollinK23 <kimballcollin@gmail.com>
2026-09-10 04:26:57 -0700

    pt 2

--- frontend/src/components/LandingPage/aboutImages.js
+++ frontend/src/components/LandingPage/aboutImages.js
@@ -1,10 +1,10 @@
 export const aboutImages = [
   { src: "/about/IMG_6295.jpg", alt: "Bracelet design detail" },
-  { src: "/about/IMG_6289.PNG", alt: "Apparel detail" },
+  { src: "/about/IMG_6289.png", alt: "Apparel detail" },
   { src: "/about/IMG_6291.png", alt: "Creative work in progress" },
   { src: "/about/IMG_6287.png", alt: "Fashion detail" },
   { src: "/about/IMG_6290.png", alt: "Lauren with a friend" },
-  { src: "/about/IMG_6292.PNG", alt: "Textile and garment detail" },
+  { src: "/about/IMG_6292.png", alt: "Textile and garment detail" },
   { src: "/about/IMG_6293.png", alt: "Portrait from Lauren's design work" },
   { src: "/about/IMG_6179.jpg", alt: "Creative studio scene" },
   { src: "/about/IMG_6288.jpg", alt: "Design inspiration" },
