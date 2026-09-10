07c7a15bb05c8597422d206023ea5d94b7e7ef38
CollinK23 <kimballcollin@gmail.com>
2026-09-10 04:22:17 -0700

    fix images not loading

--- frontend/src/components/LandingPage/aboutImages.js
+++ frontend/src/components/LandingPage/aboutImages.js
@@ -6,6 +6,6 @@ export const aboutImages = [
   { src: "/about/IMG_6290.png", alt: "Lauren with a friend" },
   { src: "/about/IMG_6292.PNG", alt: "Textile and garment detail" },
   { src: "/about/IMG_6293.png", alt: "Portrait from Lauren's design work" },
-  { src: "/about/IMG_6179.JPG", alt: "Creative studio scene" },
+  { src: "/about/IMG_6179.jpg", alt: "Creative studio scene" },
   { src: "/about/IMG_6288.jpg", alt: "Design inspiration" },
 ];
